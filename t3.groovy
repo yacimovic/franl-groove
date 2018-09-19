04fcb8dac2873d14e290ea45272b09cc47cc3998
Yugo <yacimovic@interac.ca>
2018-09-19 13:49:38 -0400

    initial test depo

---
+++ t3.groovy
@@ -0,0 +1,20 @@
+package javaposse.jobdsl.plugin.ExecuteDslScripts
+
+import javaposse.jobdsl.plugin.ExecuteDslScripts;
+
+def f=namespace(lib.FormTagLib)
+if (instance == null) {
+    instance = new ExecuteDslScripts()
+}
+
+f.radioBlock(name: 'scriptLocation', value: 'true', title: 'Use the provided DSL script', checked: instance.usingScriptText) {
+    f.entry(title: 'DSL Script', field: 'scriptText') {
+        // TODO CodeMirror support for text/x-groovy. It was unclear how do it from a .groovy stapler script
+        f.textarea(style: 'width:100%; height:10em')
+    }
+}
+f.radioBlock(name: 'scriptLocation', value: 'false', title: 'Look on Filesystem', checked: !instance.usingScriptText) {
+    f.entry(title: 'DSL Scripts', field: 'targets') {
+        f.expandableTextbox()
+    }
+}
---
+++ t4.groovy
@@ -0,0 +1,6 @@
+import lib.LayoutTagLib
+
+l=namespace(LayoutTagLib)
+div(
+  "This plugin allows Jobs to be defined via DSLs"
+)
